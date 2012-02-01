6d70b59619a778ed413fa3550492913afdc19aa7
Omega Weapon <OmegaPhil@gmail.com>
2012-02-01 21:41:06 +0000

    Moved basic info around, GPLv3 not 3+.

--- animecheck.py
+++ animecheck.py
@@ -4,13 +4,10 @@
 Version 0.3 2012.02.01
 Copyright (c) 2009, Taoufik El Aoumari (v0.2)
 Copyright (c) 2012, OmegaPhil (v0.3-)
-Released under the GPL license http://www.gnu.org/licenses/gpl-3.0.txt
-Original source: http://agafix.org/anime-crc32-checksum-in-linux-v20/
  
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+the Free Software Foundation, either version 3 of the License.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,6 +17,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
+# TODO: GPL3+?
 
 import sys, re, zlib, os
 
