7de4bdb819e17008e6155fda14513b001cef22b3
Omega Weapon <OmegaPhil@gmail.com>
2012-02-18 18:24:32 +0000

    Update future import

--- animecheck.py
+++ animecheck.py
@@ -21,8 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # TODO: GPL3+? Original author hasn't responded to emails
 
 # Preparing for Python 3
-from __future__ import division
-from __future__ import print_function
+from __future__ import division, print_function
 
 import codecs
 import hashlib
