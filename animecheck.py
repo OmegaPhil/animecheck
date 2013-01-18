612e7064bbd45fd3311c5d8be9f5e7d61b77888a
Omega Weapon <OmegaPhil@gmail.com>
2013-01-18 21:19:48 +0000

    Remove unused import

--- animecheck.py
+++ animecheck.py
@@ -31,7 +31,6 @@ This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 '''
 
-import codecs
 import hashlib
 import io
 import os
